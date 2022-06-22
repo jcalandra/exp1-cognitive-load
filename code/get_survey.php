4779c7e5072fab9169ee4c9a41c507ddd3cd553d
Joséphine Calandra <jmo.calandra@gmail.com>
2022-06-22 11:56:16 +0200

    survey

--- code/get_survey.php
+++ code/get_survey.php
@@ -7,19 +7,31 @@
 
         $answers = fopen("data".$ecoid."/resultats_".$fileid.".csv", "a");
         if(!empty($_POST["age"])){
-           fwrite($answers,$_POST["age"]);
+           fwrite($answers,"Age;".$_POST["age"]);
            fwrite($answers,"\n");
         }
         if(!empty($_POST["gender"])){
-           fwrite($answers,$_POST["gender"]);
+           fwrite($answers,"Genre;".$_POST["gender"]);
            fwrite($answers,"\n");
         }
-        if(!empty($_POST["xpjv"])){
-           fwrite($answers,"Jamais/Tres souvent;".$_POST["xpjv"]);
+        if(!empty($_POST["fm"])){
+           fwrite($answers,"Durée formation musicale;".$_POST["fm"]);
            fwrite($answers,"\n");
         }
-        if(!empty($_POST["xpbt"])){
-           fwrite($answers,"Silencieux/Assourdissant;".$_POST["xpbt"]);
+        if(!empty($_POST["dpi"])){
+           fwrite($answers,"Durée pratique d'un instrument;".$_POST["dpi"]);
+           fwrite($answers,"\n");
+        }
+        if(!empty($_POST["pm"])){
+           fwrite($answers,"Pratique musicale actuelle;".$_POST["pmo"]);
+           fwrite($answers,"\n");
+        }
+        if(!empty($_POST["ptm"])){
+           fwrite($answers,"Fréquence/semaine;".$_POST["ptm"]);
+           fwrite($answers,"\n");
+        }
+        if(!empty($_POST["pa"])){
+           fwrite($answers,"Problème d'audition;".$_POST["pa"]);
            fwrite($answers,"\n");
         }
         if(!empty($_POST["admail"])){
