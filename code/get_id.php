a03aa6ae5c02336170ea67032c3fc9e0b8d68853
jcalandra <jmo.calandra@gmail.com>
2022-06-25 22:25:21 +0200

    Delete get_id.php

--- code/get_id.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-if($_SERVER["REQUEST_METHOD"] == "GET") {
-$url_components = parse_url($_SERVER['HTTP_REFERER']);
-        parse_str($url_components['query'], $params);
-        $fileId = $params['resid'];
-        echo $fileId;
-}
-?>
